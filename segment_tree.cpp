3825204b43b3407bfa800b22cf506c1d39e8ef31
Nitin Maheshwari <138021137+maheshwari-nitin@users.noreply.github.com>
2026-01-14 00:28:06 +0530

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,43 +1,75 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-const int N = 200000;
-
-vector <int> tree(2 * N);
-
-int n;
-
-void build(vector <int> & arr){
-	for(int i = 0; i < n; i++){
-		tree[n + i] = arr[i];
-	}
-
-	for (int i = n - 1; i > 0; --i){
-		tree[i] = tree[i << 1] + tree[i << 1 | 1];
-	}
-}
-
-void updateNode(int p, int value){
-	tree[p + n] = value;
-	p = p + n;
-
-	for (int i = p; i > 1; i >>= 1)
-        tree[i>>1] = tree[i] + tree[i^1];
-}
-
-int query(int l, int r) 
-{ 
-    int res = 0;
-    
-    // query logic 
-    
-    return res;
-}
-
-
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	return 0;
-}
+template <typename T>
+class SegmentTree {
+private:
+    int size;
+    vector<T> t;
+    T Id; // Neutral element for invalid range
+    function<T(T, T)> combine;
+ 
+    void build(vector<T>& a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v * 2, tl, tm);
+            build(a, v * 2 + 1, tm + 1, tr);
+            t[v] = combine(t[v * 2], t[v * 2 + 1]);
+        }
+    }
+ 
+    T get(int v, int tl, int tr, int l, int r) {
+        if (l > r) return Id;
+        if ((tl == l) && (tr == r)) {
+            return t[v];
+        }
+        int tm = (tl + tr) / 2;
+        return combine(
+            get(v * 2, tl, tm, l, min(r, tm)),
+            get(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r)
+        );
+    }
+ 
+    void update(int v, int tl, int tr, int pos, T new_val) { // point update
+        if (tl == tr) {
+            t[v] = new_val;
+        } else {
+            int tm = (tl + tr) / 2;
+            if (pos <= tm)
+                update(v * 2, tl, tm, pos, new_val);
+            else
+                update(v * 2 + 1, tm + 1, tr, pos, new_val);
+            t[v] = combine(t[v * 2], t[v * 2 + 1]);
+        }
+    }
+ 
+public:
+    SegmentTree(function<T(T, T)> combineFunction, T Id)
+    : combine(combineFunction), Id(Id) {}
+ 
+    void init(int n) {
+        size = 1;
+        while (size < n) size *= 2;
+        // t.resize(2 * size);
+        t.assign(2 * size, Id); // if no build
+    }
+ 
+    void build(vector<T>& a) {
+        a.resize(size, Id); // Resize and fill with default value
+        build(a, 1, 0, size - 1);
+    }
+ 
+    T get(int l, int r) {
+        return get(1, 0, size - 1, l, r);
+    }
+ 
+    void update(int pos, T inc) { // assignment, can also make for increment
+        update(1, 0, size - 1, pos, inc);
+    }
+ 
+    void print() {
+        for (int i = 1; i < 2 * size; ++i) {
+            cout << t[i] << " ";
+        }
+        cout << endl;
+    }
+};
